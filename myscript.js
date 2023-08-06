d62cf64328b3be3c856f9fb125f39194fea55095
Rina Rasia <rinarasia@gmail.com>
2023-08-06 16:24:45 -0700

    Add buttons to  play single game and display score

--- myscript.js
+++ myscript.js
@@ -1,24 +1,25 @@
 // Define choices
 const choices = ["rock","paper", "scissors"];
 
-// Get player and computer choices
-const playerSelection = prompt("Rock, paper, scissors?").toLowerCase();
-const computerSelection = getComputerChoice();
-
 // Initialize Player and Computer score
 let playerScore = 0;
 let computerScore = 0;
 
-// Randomize Computer choice
-function getComputerChoice() {
-    return choices[Math.floor(Math.random() * choices.length)];
-}
 
 // Play single round
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+
+    const computerSelection = getComputerChoice();
+    
+    // Randomize Computer choice
+    function getComputerChoice() {
+        return choices[Math.floor(Math.random() * choices.length)];
+    }
+
     const win = `You win! Your choice ${playerSelection} beats ${computerSelection}.`;
     const lose = `Oh no, you lost. Computer's ${computerSelection} beats your ${playerSelection}.`;
     const tie = `You have tied with Computer. ${playerSelection} is the same as ${computerSelection}`;
+    
     if (playerSelection == "rock") {
         switch (computerSelection) {
             case "rock":
@@ -63,7 +64,7 @@ function playRound(playerSelection, computerSelection) {
         }
     }
 }
-
+/*
 // Play game of 5 rounds
 function game() {
     for (let i = 1; i <= 5; i++) {
@@ -83,3 +84,31 @@ function game() {
 
 // Call game to start
 game();
+*/
+
+
+window.onload = function() {
+    const rockBtn = document.querySelector('#rock');
+    rockBtn.addEventListener('click', playRock);
+
+    const paperBtn = document.querySelector('#paper');
+    paperBtn.addEventListener('click', playPaper);
+
+    const scissorsBtn = document.querySelector('#scissors');
+    scissorsBtn.addEventListener('click', playScissors);
+};
+
+function playRock() {
+    console.log(playRound('rock'));
+    document.getElementById("buttons").innerHTML = `Player Score: ${playerScore}\nComputer Score: ${computerScore}`;
+};
+
+function playPaper() {
+    console.log(playRound('paper'));
+    document.getElementById("buttons").innerHTML = `Player Score: ${playerScore}\nComputer Score: ${computerScore}`;
+};
+
+function playScissors() {
+    console.log(playRound('scissors'));
+    document.getElementById("buttons").innerHTML = `Player Score: ${playerScore}\nComputer Score: ${computerScore}`;
+};
